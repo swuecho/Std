cba3295fbb9d111d71a4f434c49fbdc02047d3df
Hao Wu <echowuhao@gmail.com>
2017-05-16 19:34:40 -0400

    initial

--- lib/Jieba.pm
+++ lib/Jieba.pm
@@ -1,24 +1,18 @@
 package Jieba;
 use v5.10;
+use Lingua::ZH::Jieba;
 use parent 'Exporter';
 our @EXPORT = qw/jieba_tokenize test/;
 
+my $jieba = Lingua::ZH::Jieba->new();
+
 sub jieba_tokenize {
-    jieba_tokenize_python(shift);
+    my $words_cut4search = $jieba->cut_for_search(shift);
+    return $words_cut4search;
 }
 
 sub test {
     "hello world"
 }
-# TODO: 
-#result = jieba.tokenize(u'永和服装饰品有限公司', mode='search')
-use Inline Python => <<'END_OF_PYTHON_CODE';
-from jieba import tokenize
-
-def jieba_tokenize_python(text):
-    seg_list = tokenize(text, mode='search')
-    return(list(seg_list))
-
-END_OF_PYTHON_CODE
 
 1;
